8cd2ba0e73b4dfe52fef96878810b27025e1bde5
Yi Hu <yathu@google.com>
2024-04-16 11:15:53 -0400

    update

--- beamtest/src/main/java/com/github/abacn/MinimumStreaming.java
+++ beamtest/src/main/java/com/github/abacn/MinimumStreaming.java
@@ -8,7 +8,7 @@ import org.apache.beam.sdk.transforms.Create;
 // ./gradlew :beamtest:run --args='--project=google.com:clouddfe --region=us-central1 --tempLocation=gs://clouddfe-yihu-test/tmp --runner=DataflowRunner --streaming'
 //
 // ./gradlew :beamtest:shadowJar
-// ./gradlew :beamtest:run --args='--project=google.com:clouddfe --region=us-central1 --tempLocation=gs://clouddfe-yihu-test/tmp --runner=DataflowRunner --filesToStage=/Users/yathu/dev/piece/beam-demo/beamtest/build/libs/beamtest-1.0-all.jar'
+// ./gradlew :beamtest:run --args='--project=google.com:clouddfe --region=us-central1 --tempLocation=gs://clouddfe-yihu-test/tmp --runner=DataflowRunner --experiments=use_runner_v2 --filesToStage=/Users/yathu/dev/piece/beam-demo/beamtest/build/libs/beamtest-1.0-all.jar'
 public class MinimumStreaming {
 
   public static void main(String[] argv) {
